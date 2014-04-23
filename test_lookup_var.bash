4ecbc32952d6e43ead15c58b7dd6a921c37c863b
Eric Minikel <ericminikel@gmail.com>
2014-04-23 12:13:44 -0400

    re-ordered command line vars to match lookup_var.py

--- test_lookup_var.bash
+++ test_lookup_var.bash
@@ -5,23 +5,18 @@ na12878_joint_all=/seq/dax/macarthur_joint_calling/v1/macarthur_joint_calling.si
 full_86k_vcf=#***real file location deliberately obscured***
 reftable_86k=/humgen/atgu1/fs03/eminikel/045atgu/minrep/86k_gvcfs_and_bams.txt
 
-# example 1: not using reftable. note use of NA where reftable param would go
-lookup_var.py $na12878_alone_all NA 1 1827835 CTTGG CTGG
-# output:
-# # You searched for:  1827835 CT C
-# # Relevant line from VCF:  1 1827835 . CT CT,C 9402 PASS set=Intersection GT
-# #SAMPLE	CALL
-# NA12878	2|1
+# example 1: not using reftable. 
+lookup_var.py 1 1827835 CTTGG CTGG $na12878_alone_all 
 
 # further examples using reftable against 86k dataset
 
 # three ways to find the same variant
-lookup_var.py $full_86k_vcf $reftable_86k 1 861223 AG CG # as found in reference VCF
-lookup_var.py $full_86k_vcf $reftable_86k 1 861222 TAG TCG # a different non-minimal representation of same variant
-lookup_var.py $full_86k_vcf $reftable_86k 1 861223 A C # searching for the minimal representation
+lookup_var.py 1 861223 AG CG $full_86k_vcf -t $reftable_86k # as found in reference VCF
+lookup_var.py 1 861222 TAG TCG $full_86k_vcf -t $reftable_86k # a different non-minimal representation of same variant
+lookup_var.py 1 861223 A C $full_86k_vcf -t $reftable_86k # searching for the minimal representation
 
 # three ways to find another variant
-lookup_var.py $full_86k_vcf $reftable_86k 1 878677 CCT TCT # as found in reference VCF
-lookup_var.py $full_86k_vcf $reftable_86k 1 878676 ACCT ATCT
-lookup_var.py $full_86k_vcf $reftable_86k 1 878677 C T # searching for minimal representation
+lookup_var.py 1 878677 CCT TCT $full_86k_vcf -t $reftable_86k # as found in reference VCF
+lookup_var.py 1 878676 ACCT ATCT $full_86k_vcf -t $reftable_86k 
+lookup_var.py 1 878677 C T $full_86k_vcf -t $reftable_86k  # searching for minimal representation
 
